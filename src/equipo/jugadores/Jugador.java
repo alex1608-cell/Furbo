61d3cc397fcd2fbe15c3bf5c9e4e958542ec093f
Alex <alejc2006@gmail.com>
2025-03-04 10:18:48 +0100

    miau

--- src/equipo/jugadores/Jugador.java
+++ src/equipo/jugadores/Jugador.java
@@ -1,5 +1,46 @@
 package equipo.jugadores;
 
 public class Jugador {
+<<<<<<< Updated upstream
 
+=======
+	private String nombre;
+	private int dorsal;
+	private String equipo;
+	
+	public Jugador(String nombre, int dorsal, String equipo) {
+		
+		this.dorsal = dorsal;
+		this.equipo = equipo;
+		this.nombre = nombre;
+		
+		
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getDorsal() {
+		return dorsal;
+	}
+
+	public void setDorsal(int dorsal) {
+		this.dorsal = dorsal;
+	}
+
+	public String getEquipo() {
+		return equipo;
+	}
+
+	public void setEquipo(String equipo) {
+		this.equipo = equipo;
+	}
+	
+	
+>>>>>>> Stashed changes
 }
